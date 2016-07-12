0df12bda4669bf8e417ebed24750056290bbdb9d
sakura-pc <841105127@qq.com>
2016-07-12 22:48:22 +0800

    change

--- src/components/Main.js
+++ src/components/Main.js
@@ -45,7 +45,6 @@ var ImgFigure=React.createClass({
             ['MozTransform','MsTransform','WebkitTransform',''].forEach(function(value){
                 styleObj[value]='rotate('+this.props.arrange.rotate +'deg)';
             }.bind(this));
-
         }
         if(this.props.arrange.isCenter){
             styleObj.zIndex=11;
@@ -70,33 +69,33 @@ var ImgFigure=React.createClass({
     }
 });
 // 控制组件
-var ControllerUnit=React.createClass({
-    handleClick:function(e){
-        // 如果点击的是当前正在选中态的按钮，则翻转图片，否则将对应的图片距仲话
-        if(this.props.arrange.isCenter){
-            this.props.inverse();
-        }else{
-            this.props.center();
-        }
-        e.stopPropagation();
-        e.preventDefault();
-    },
-    render:function(){
-        var ControllerUnitClassName="controller-unit";
-        // 如果对应的是居中的图片，显示控制按钮的居中态
-        if(this.props.arrange.isCenter){
-            ControllerUnitClassName+=" is-center";
-
-            // 如果同时对应的是翻转图片，显示控制按钮的翻转态
-            if(this.props.arrange.isInverse){
-                ControllerUnitClassName+=" is-inverse";
-            }
-        }
-        return (
-            <span className={ControllerUnitClassName} onClick={this.handleClick}></span>
-        );
-    }
-});
+// var ControllerUnit=React.createClass({
+//     handleClick:function(e){
+//         // 如果点击的是当前正在选中态的按钮，则翻转图片，否则将对应的图片距仲话
+//         if(this.props.arrange.isCenter){
+//             this.props.inverse();
+//         }else{
+//             this.props.center();
+//         }
+//         e.stopPropagation();
+//         e.preventDefault();
+//     },
+//     render:function(){
+//         var ControllerUnitClassName="controller-unit";
+//         // 如果对应的是居中的图片，显示控制按钮的居中态
+//         if(this.props.arrange.isCenter){
+//             ControllerUnitClassName+=" is-center";
+//
+//             // 如果同时对应的是翻转图片，显示控制按钮的翻转态
+//             if(this.props.arrange.isInverse){
+//                 ControllerUnitClassName+=" is-inverse";
+//             }
+//         }
+//         return (
+//             <span className={ControllerUnitClassName} onClick={this.handleClick}></span>
+//         );
+//     }
+// });
 var AppComponent= React.createClass({
     Constant:{
         cenerPos:{
@@ -272,7 +271,7 @@ var AppComponent= React.createClass({
                 }
             }
             imgFigures.push(<ImgFigure key={index} data={value} ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>);
-            controllerUnits.push(<ControllerUnit key={index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>);
+            // controllerUnits.push(<ControllerUnit key={index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>);
         }.bind(this));
         return (
             <section className="stage" ref="stage">
